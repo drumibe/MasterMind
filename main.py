51757e5fa9f0f907609404747a1d5760e2f83e4a
mathmatcher <micah@tambour.io>
2020-03-31 17:18:29 +0000

    levels

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 password_cracked = False
 password = []
 guess = []
+level = 1
+import pop
 def generate_password():
-  global password
-  for i in range(5):
+  global password, level
+  for i in range(level):
     import random
     passwordPart = random.randint(0, 9)
     password.append(passwordPart)
 def ask_for_guess():
-  global guess
-  for i in range(5):
+  global guess, level
+  for i in range(level):
     user = input("chose a number from 0 to 9. ")
     num = int(user)
     guess.append(num)
   print(guess)
 def password_check_if_any_is_incorect():
-  global password, guess
+  global password, guess, level
   guessIndex = 0
-  for i in range(5):
+  for i in range(level):
     gussNum = i + 1
     if guess[guessIndex] == password[guessIndex]:
       hint = "correct"
@@ -26,10 +28,10 @@ def password_check_if_any_is_incorect():
     print("guss number ", gussNum, hint)
     guessIndex += 1
   guessIndex = 0
-  for i in range(5):
+  for i in range(level):
     guess.pop()
 def password_check():
-  global password, guess, password_check_if_any_is_incorect, password_cracked
+  global password, guess, password_check_if_any_is_incorect, password_cracked, level
   if password == guess:
     print("congrats you did it!")
     password_cracked = True
@@ -37,11 +39,17 @@ def password_check():
     password_check_if_any_is_incorect()
 generate_password()
 #gessing Time
-while password_cracked == False:
-  ask_for_guess()
-  password_check()
-  
+while True:
+  while password_cracked == False:
+    ask_for_guess()
+    password_check()
+  password = []
+  guess = []
+  level += 1
+  generate_password()
+  password_cracked = False
 
+  
 
 
 
